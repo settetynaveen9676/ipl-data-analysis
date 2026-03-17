36178be6a4c913193a654b9e23235e13af3426e2
settetynaveen9676 <settetynaveenkumar@gmail.com>
2026-03-17 23:39:13 +0530

    analysis.py

--- analysis.py
+++ analysis.py
@@ -1,11 +1,18 @@
 import pandas as pd
+import matplotlib.pyplot as plt
 
 matches = pd.read_csv("matches.csv")
 
 # Top teams
-print("Top Teams:")
-print(matches["winner"].value_counts().head(10))
+top_teams = matches["winner"].value_counts().head(5)
+print("Top Teams:\n", top_teams)
 
 # Top players
-print("\nTop Players:")
-print(matches["player_of_match"].value_counts().head(10))
+top_players = matches["player_of_match"].value_counts().head(5)
+print("\nTop Players:\n", top_players)
+
+# Visualization
+top_teams.plot(kind='bar', title="Top IPL Teams")
+plt.xlabel("Teams")
+plt.ylabel("Wins")
+plt.show()
